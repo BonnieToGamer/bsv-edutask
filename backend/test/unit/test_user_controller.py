53a88d156a93a454880b34b12ccfe2cb2be90406
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2026-04-15 12:31:32 +0200

    added test no found user

--- backend/test/unit/test_user_controller.py
+++ backend/test/unit/test_user_controller.py
@@ -14,3 +14,15 @@ def test_get_user_by_email_invalid():
         usercontrol.get_user_by_email("Email")
         
 
+@pytest.mark.unit
+def test_get_user_by_email_valid_email_no_user():
+    # arrange
+    dao_mock = MagicMock()
+    dao_mock.find.return_value = [None]
+
+    # act
+    user_controller = UserController(dao_mock)
+    result = user_controller.get_user_by_email("email@example.com")
+
+    # assert
+    assert result is None
