0e9405712f23e4d1bd5962e069ccf73d2fc5effb
Mikko <mikkooonttonen@gmail.com>
2026-04-15 12:30:10 +0200

    added test for invalid email

---
+++ backend/test/unit/test_get_email_by_user.py
@@ -0,0 +1,16 @@
+import pytest
+from unittest.mock import MagicMock
+from src.controllers.usercontroller import UserController
+
+@pytest.mark.unit
+def test_get_user_by_email_invalid():
+    #Arrange
+    dao_mock = MagicMock()
+    dao_mock.find.return_value = None
+    #Act  
+    usercontrol = UserController(dao_mock)
+    #Assert 
+    with pytest.raises(ValueError):
+        usercontrol.get_user_by_email("Email")
+        
+
