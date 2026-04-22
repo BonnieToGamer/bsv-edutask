726f6879dc26f3c32920c27824439b651b710208
420k <420k@420k.com>
2026-04-22 12:15:23 +0200

    added tests for missing fields in doc and minimal valid doc

--- backend/test/integration/test_dao_create.py
+++ backend/test/integration/test_dao_create.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
 from bson import ObjectId
-
+from pymongo.errors import WriteError
 from src.util.dao import DAO
 
 def create_clear_dao(collection_name: str) -> DAO:
@@ -54,3 +54,36 @@ def test_full_valid(dao):
         {"$oid": str(task_id1)},
         {"$oid": str(task_id2)}
     ]
+    
+    
+@pytest.mark.integration 
+def test_minimal_valid_doc(dao):
+    # arrange
+    new_user = {
+        "firstName": "Mattias",
+        "lastName": "Larsson",
+        "email": "mattias.larsson@example.com"
+    }
+    
+    # act
+    inserted_user = dao.create(new_user)
+
+    # assert
+    assert inserted_user != None
+    assert inserted_user["firstName"] == new_user["firstName"]
+    assert inserted_user["lastName"] == new_user["lastName"]
+    assert inserted_user["email"] == new_user["email"]
+    
+    
+@pytest.mark.integration 
+def test_missing_fields_doc(dao):
+
+    with pytest.raises(WriteError):
+        # arrange
+        new_user = {
+            "firstName": "Mattias",
+            "lastName": "Larsson",
+        }
+        
+        # act
+        inserted_user = dao.create(new_user)
